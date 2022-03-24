cf4738053c4ba35aaf3ebeb01b7a6355da01d95d
Mark-Palfi <99292598+Mark-Palfi@users.noreply.github.com>
2022-03-24 21:26:25 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,35 @@
+// Created by: Mark Palfi
+// Created on: Mar 2022
+//
+// This program does math 
+
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        // This function accepts user input
+        string street_name;
+        int street_number;
+
+        Console.WriteLine("This program calculates the area and perimeter of a triangle.");
+        Console.WriteLine("");
+
+        Console.Write("Enter side lengh 1: ");
+        side1 = Console.ReadLine();
+        Console.Write("Enter side lengh 2: ");
+        side2 = Console.ReadLine();
+        Console.Write("Enter side lengh 3: ");
+        side3 = Console.ReadLine();
+        Console.Write("Enter height: ");
+        height = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(2 + 6 * 4);
+        Console.ReadLine();
+        
+        Console.WriteLine("");
+        Console.WriteLine("".");
+
+        Console.WriteLine("\nDone.");
+    }
+}
